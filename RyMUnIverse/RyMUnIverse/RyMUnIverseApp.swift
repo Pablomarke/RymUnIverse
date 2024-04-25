e30df57c0e8ec7cbc70a2684665d1c7df7e1b80c
Pablomarke <pablomarquezartesano@gmail.com>
2024-04-25 14:49:27 +0200

    create app

---
+++ RyMUnIverse/RyMUnIverse/RyMUnIverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  RyMUnIverseApp.swift
+//  RyMUnIverse
+//
+//  Created by Pablo Márquez Marín on 25/4/24.
+//
+
+import SwiftUI
+
+@main
+struct RyMUnIverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
